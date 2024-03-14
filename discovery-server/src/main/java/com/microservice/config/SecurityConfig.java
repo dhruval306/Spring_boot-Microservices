dc60c50f5768b296e67f12346c11e566f1d7e771
dhruval <dhruval.soni306@gmail.com>
2024-03-14 16:42:05 -0300

    csrf Deprecated: solved

--- discovery-server/src/main/java/com/microservice/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/microservice/config/SecurityConfig.java
@@ -1,16 +1,19 @@
 package com.microservice.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
-        return httpSecurity.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> {
+            csrf.ignoringRequestMatchers(request -> {
+                String path = request.getServletPath();
+                return path.startsWith("/eureka/");
+            });
+        });
+        return http.build();
     }
 }
